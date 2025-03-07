83cbe57f627c3edeccfd4ea088d05ae94f56e851
AMG <andresmargar98@gmail.com>
2025-03-07 23:21:10 +0100

    initial

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "org.example.AppKt"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ app/src/test/kotlin/org/example/AppTest.kt
@@ -0,0 +1,12 @@
+package org.example
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertNotNull
+
+class AppTest {
+    @Test
+    fun appHasAGreeting() {
+        val classUnderTest = App()
+        assertNotNull(classUnderTest.greeting, "app should have a greeting")
+    }
+}
