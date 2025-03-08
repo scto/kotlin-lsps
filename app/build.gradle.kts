13b03fcf26ec268f2c07d8c0aa79b140bfb147ff
AMG <andresmargar98@gmail.com>
2025-03-08 14:37:37 +0100

    environment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+val analysisApiKotlinVersion = "2.1.20-dev-3305"
+val intellijVersion = "233.13135.128"
+
 plugins {
     kotlin("jvm") version "2.1.0"
     application
@@ -5,9 +8,33 @@ plugins {
 
 repositories {
     mavenCentral()
+
+    maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
+    maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
+    maven(url = "https://www.jetbrains.com/intellij-repository/releases")
+    maven(url = "https://cache-redirector.jetbrains.com/intellij-third-party-dependencies")
 }
 
 dependencies {
+    implementation("com.jetbrains.intellij.platform:core:$intellijVersion")
+    implementation("com.jetbrains.intellij.platform:core-impl:$intellijVersion")
+    implementation("com.jetbrains.intellij.platform:util:$intellijVersion")
+    implementation("org.jetbrains.kotlin:kotlin-compiler:$analysisApiKotlinVersion")
+    implementation("com.github.ben-manes.caffeine:caffeine:2.9.3")  // Needed by kotlin analysis api
+    listOf(
+        "org.jetbrains.kotlin:high-level-api-fir-for-ide",
+        "org.jetbrains.kotlin:analysis-api-platform-interface-for-ide",
+        "org.jetbrains.kotlin:high-level-api-for-ide",
+        "org.jetbrains.kotlin:low-level-api-fir-for-ide",
+        "org.jetbrains.kotlin:symbol-light-classes-for-ide",
+        "org.jetbrains.kotlin:high-level-api-impl-base-for-ide",
+        "org.jetbrains.kotlin:kotlin-compiler-common-for-ide",
+        "org.jetbrains.kotlin:kotlin-compiler-fir-for-ide",
+        "org.jetbrains.kotlin:kotlin-compiler-ir-for-ide",
+    ).forEach {
+        implementation("$it:$analysisApiKotlinVersion") { isTransitive = false }
+    }
+
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,12 +1,19 @@
 package org.example
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
-}
+import com.intellij.openapi.util.Disposer
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreApplicationEnvironmentMode
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.cli.jvm.compiler.setupIdeaStandaloneExecution
+import org.jetbrains.kotlin.config.CompilerConfiguration
 
 fun main() {
-    println(App().greeting)
+    setupIdeaStandaloneExecution()
+    val projectDisposable = Disposer.newDisposable("StandaloneAnalysisAPISession.project")
+    val compilerConfiguration = CompilerConfiguration()
+    val environment = KotlinCoreEnvironment.getOrCreateApplicationEnvironment(
+        projectDisposable,
+        compilerConfiguration,
+        KotlinCoreApplicationEnvironmentMode.Production,
+    )
+    println("Hello, world!")
 }
